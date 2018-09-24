8f435586d4fa380696ad63d1ca5eb53806394033
Stephen Axtell <axtell.stephen@gmail.com>
2018-09-24 19:17:38 +0200

    implement Search controller

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,32 @@
 import Search from './models/Search';
 
-const search = new Search('pizza');
-console.log(search);
-search.getResults();
+/* Here we are going to set a global state,
+  Search object
+  Current recipe object
+  Shopping list object
+  Liked recipe */
+
+const state = {
+
+};
+
+const controlSearch = async () => {
+  // Get query from view
+  const query = 'pizza';
+
+  if (query) {
+    // New search object and add to state
+    state.search = new Search(query);
+  }
+
+  // Search for recipes
+  await state.search.getResults();
+
+  // Render result on ui
+  console.log(state.search.results);
+};
+
+document.querySelector('.search').addEventListener('submit', e => {
+  e.preventDefault();
+  controlSearch();
+});
